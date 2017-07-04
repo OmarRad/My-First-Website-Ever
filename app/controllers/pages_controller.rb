23f4de2bf30eabd0172f22995b17d5d25f92b8ed
OmarRad <oakz1999@gmail.com>
2017-07-03 20:42:42 -0400

    first commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,9 @@
+class PagesController < ApplicationController
+
+	def home
+		
+
+		render("home")
+	end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+
+	get "/home" => "pages#home"
+
+	root "pages#home"
+
+end
